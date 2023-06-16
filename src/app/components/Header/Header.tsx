b762833ece16cb406a463ad099f71de5c5ee0037
Alexander Kim <aksovius@naver.com>
2023-06-16 10:48:34 +0900

    fead: add side menu resize listener

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,9 +1,13 @@
 'use client';
 import { SideMenuProps } from '@/app/types/SideMenu';
 import styles from './header.module.css';
+import { useEffect } from 'react';
 
 
 export default function Header({isOpen, setIsOpen}: SideMenuProps) {
+
+   
+
     return <div className={styles.header} id='header'>
         <button id='side-menu-button' className={styles.button} onClick={() => setIsOpen(!isOpen)} >{isOpen ? "x" : "="}</button>
         <h1>Header</h1>
