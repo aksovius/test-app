0bedaa20529a087cd79abe7004f378b93de8c252
Alexander Kim <64238640+aksovius@users.noreply.github.com>
2023-07-02 17:51:35 +0900

    Style: header

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -4,6 +4,6 @@ import styles from './header.module.css';
 export default function Header({ isOpen, setIsOpen }: SideMenuProps) {
     return <div className={styles.header} id='header'>
         <button id='side-menu-button' className={styles.button} onClick={() => setIsOpen(!isOpen)} >{isOpen ? "\u{0078}" : "\u{2630}"}</button>
-        <h1>Kakao Brain 사전과제</h1>
+        <h1>Test app</h1>
     </div>
 }
